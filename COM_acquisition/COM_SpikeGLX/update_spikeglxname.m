8d3ebb698e9d3e95e75a6299b899a71d393e4213
nielsenlab <nielsenlabmbi@gmail.com>
2026-03-24 16:34:59 -0400

    spikeglx filenames

--- COM_acquisition/COM_SpikeGLX/update_spikeglxname.m
+++ COM_acquisition/COM_SpikeGLX/update_spikeglxname.m
@@ -2,7 +2,17 @@ function update_spikeglxname
 
 global Mstate GcomState 
 
-
+%get name
 name = [Mstate.anim '_u' Mstate.unit '_' Mstate.expt];
 
+%make directory using shared drive
+datadir=fullfile('/mnt','windows_share',Mstate.anim,name);
+s=mkdir(datadir);
+if s==0
+    disp('Error: Could not create data directory!')
+end
+
+%set filename (on windows machine)
 SetRunName(GcomState, name);
+SetNextFileName(GcomState,['D:\' Mstate.anim '\' name '\' name]);
+%SetRunName(GcomState, name);
